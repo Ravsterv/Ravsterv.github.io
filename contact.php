c943de386e471091cfe91d990d53d44c61d6fbe3
Ravsterv <50719718+Ravsterv@users.noreply.github.com>
2019-05-23 17:02:46 +1000

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<body>
+<head>
+<link rel="stylesheet" type="text/css" href="Exploitsstylesheet.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<h2>Thank for your submission.</h2>
+</head>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+<a href='index.html'>Home</a>
+</html>
